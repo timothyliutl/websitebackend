f370959011dababf1119c3b89226080d22ed8f9e
timothyliutl <18tfl1@queensu.ca>
2021-01-23 14:05:45 -0500

    few more edits

--- signin.js
+++ signin.js
@@ -1,11 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const signIn = (userName, sessionLength, jwtKey) =>{
-    console.log('hello');
-    console.log(userName);
-    console.log(sessionLength);
-    console.log(jwtKey);
-
     var token = jwt.sign({userName:userName}, jwtKey, {algorithm: "HS256", expiresIn: sessionLength});
     console.log(token)
     return(token);
